fdd4f9187389a2bc2a1ea34ab0a0b59c85c756fe
Bhoomika <bhoomika.jalandhra@gmail.com>
2026-05-25 09:56:53 +0530

    Datatypes in c++

--- datatypes.cpp
+++ datatypes.cpp
@@ -5,15 +5,30 @@ int main()
 {
     int a=123;
     cout<<a<<endl;
+
     char b='b';
     cout<<b<<endl;
+
     bool c=true;
     cout<<c<<endl;
+
     float f=1.2;
     cout<<f<<endl;
+
     double d=1.2365599;
     cout<<d<<endl;
 
     int size= sizeof(d);
     cout<<"Size of d is: "<<size<<endl;
+
+    //Typecasting
+    int n='a';
+    cout<<n<<endl;
+
+    char m=99;
+    cout<<m<<endl;
+
+    //If we store bigger value than the variable size then it will give result for the digits stored, rest it will
+    char o=123456;
+    cout<<o<<endl;
 }
